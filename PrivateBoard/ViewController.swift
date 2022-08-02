c0df008123d46497fb5c497af19823940fffb7d6
WongukYang <wollxxllow@gmail.com>
2022-08-02 14:12:30 +0900

    Initial Commit

---
+++ PrivateBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PrivateBoard
+//
+//  Created by WG Yang on 2022/08/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
