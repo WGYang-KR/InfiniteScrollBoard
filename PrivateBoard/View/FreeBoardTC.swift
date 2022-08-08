9c9db0ec5632c879fad5aa8c1b3442281deef9d9
WongukYang <wollxxllow@gmail.com>
2022-08-09 07:08:39 +0900

    [FIX]테이블셀 레이아웃 수정

--- PrivateBoard/View/FreeBoardTC.swift
+++ PrivateBoard/View/FreeBoardTC.swift
@@ -16,7 +16,7 @@ class FreeBoardTC : UITableViewCell{
     @IBOutlet weak var readCntLabel: UILabel!
     @IBOutlet weak var commentCntLabel: UILabel!
     @IBOutlet weak var thumbImageView: UIImageView!
-    
+    @IBOutlet weak var thumbImageViewContainer: UIView!
     var viewModel: FreeBoardTCVM? {
         didSet {
             bindViewModel()
@@ -31,11 +31,11 @@ class FreeBoardTC : UITableViewCell{
             readCntLabel.text = vm.readCntVM
             commentCntLabel.text = vm.commentCntVM
             if let imagePath = vm.thumbImageVM {
-                thumbImageView.isHidden = false
+                thumbImageViewContainer.isHidden = false
                 let url = URL(string: BaseURL.Image + imagePath)
                 thumbImageView.kf.setImage(with: url)
             } else {
-                thumbImageView.isHidden = true
+                thumbImageViewContainer.isHidden = true
             }
         }
     }
--- PrivateBoard/ViewModel/FreeBoardTCVM.swift
+++ PrivateBoard/ViewModel/FreeBoardTCVM.swift
@@ -21,10 +21,20 @@ protocol FreeBoardTCVM {
 
 extension Article: FreeBoardTCVM {
     
-    var contentVM: String { return boardCN ?? "" }
+    var contentVM: String { return boardSj ?? "" }
     var nicknameVM: String { return wrterNcnm ?? "글쓴이미상" }
-    var dateVM: String { return creatDt ?? "날짜미상"}
-    var readCntVM: String { return "조회수 + \(rdcnt ?? 0)"  }
+    var dateVM: String {
+        if let creatDt = creatDt {
+            let splited = creatDt.components(separatedBy: ["-",":"," "])
+            let year:String = String(Array(splited[0])[2...3])
+            let month: String = splited[1]
+            let day: String = splited[2]
+            return "\(year).\(month).\(day)"
+        } else {
+            return "날짜 미상"
+        }
+    }
+    var readCntVM: String { return "조회수 \(rdcnt ?? 0)"  }
     var commentCntVM: String { return "\(anscnt ?? 0)" }
     var thumbImageVM: String? { return userProfileFilePth }
     
